4043d0282f55fa95ec82d6660dd45dccc57a18f5
Davide Soltys <91910438+ItsAtlant@users.noreply.github.com>
2023-12-06 13:09:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import speech_recognition
 import pyautogui
-import os
-import sys
+from utils import save, close, program
 recogniser = speech_recognition.Recognizer()
 
 times = 1
@@ -17,62 +16,7 @@ comandi = {
     "start": "enter",
     "help": "i"
 }
-#start program
-def program():
-    os.startfile(r"game\visualboyadvance-m.exe")
-    pyautogui.sleep(2)
-    coordinate = pyautogui.locateOnScreen(r"source\argomenti advanced.PNG")
-    pyautogui.sleep(1)
-    pyautogui.moveTo(coordinate)
-    pyautogui.sleep(1)
-    pyautogui.leftClick(coordinate)
-    pyautogui.sleep(1)
-    coordinate = pyautogui.locateOnScreen(r"source\Open.PNG")
-    pyautogui.sleep(1)
-    pyautogui.moveTo(coordinate)
-    pyautogui.sleep(1)
-    pyautogui.leftClick(coordinate)
-    pyautogui.sleep(1)
-    try:
-        coordinate = pyautogui.locateOnScreen(r"source\gbaLightmode.PNG")
-    except pyautogui.ImageNotFoundException:
-        coordinate = pyautogui.locateOnScreen(r"source\gbaDarkmod.PNG")
 
-    pyautogui.moveTo(coordinate)
-    pyautogui.click(clicks=2, button='left')
-    pyautogui.sleep(1)
-    pyautogui.keyDown("f1")
-    pyautogui.keyUp("f1")
-
-#save
-def save():
-    pyautogui.leftClick()
-    try:
-        coordinate = pyautogui.locateOnScreen(r"source\consoleDarkmode.PNG")
-    except pyautogui.ImageNotFoundException:
-        coordinate = pyautogui.locateOnScreen(r"source\consoleLightmod.PNG")
-    pyautogui.moveTo(coordinate)
-    coordinate = pyautogui.locateOnScreen(r"source\argomenti advanced.PNG")
-    pyautogui.sleep(1)
-    pyautogui.moveTo(coordinate)
-    pyautogui.sleep(1)
-    pyautogui.leftClick(coordinate)
-
-    coordinate = pyautogui.locateOnScreen(r"source\save.PNG")
-    pyautogui.sleep(1)
-    pyautogui.moveTo(coordinate)
-    pyautogui.leftClick(coordinate)
-    coordinate = pyautogui.locateOnScreen(r"source\f1slotsave.PNG")
-    pyautogui.moveTo(coordinate)
-    pyautogui.leftClick(coordinate)
-    pyautogui.sleep(1)
-
-def close():
-    pyautogui.leftClick()
-    coordinate = pyautogui.locateOnScreen(r"source\close.PNG")
-    pyautogui.moveTo(coordinate)
-    pyautogui.leftClick(coordinate)
-    sys.exit()
 
 print("Say program to start the emulator")
 while True:
@@ -103,4 +47,5 @@ while True:
 
     except speech_recognition.UnknownValueError:
         print("repeat pls")
+
 
